a6365a97b39ed07ac6f66469c04be8f9f6155710
Lê Anh Kiệt <58032294+Anhkiet99@users.noreply.github.com>
2021-01-05 20:48:44 +0700

    Add files via upload

---
+++ BUS/TracNghiem/KyThiThuBUS.cs
@@ -0,0 +1,64 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class KyThiThuBUS
+    {
+        #region singleton
+        private static KyThiThuBUS instance;
+
+        public static KyThiThuBUS Instance
+        {
+            get { if (instance == null) instance = new KyThiThuBUS(); return KyThiThuBUS.instance; }
+            private set { KyThiThuBUS.instance = value; }
+        }
+        private KyThiThuBUS() { }
+        #endregion
+        #region method
+        public void Insert_KyThiThu(KyThiThu kyThi)
+        {
+            try
+            {
+                KyThiThuDAO.Instance.Insert_KyThiThu(kyThi);
+            }
+            catch
+            {
+
+            }
+
+        }
+        public void Insert_CT_KyThiThu(CT_KyThiThu ct)
+        {
+            try
+            {
+                KyThiThuDAO.Instance.Insert_CT_KyThiThu(ct);
+            }
+            catch
+            {
+
+            }
+
+        }
+        public void Insert_CT_HocSinh_KyThiThu(CT_HOCSINH_KyThiThu ct)
+        {
+            try
+            {
+                KyThiThuDAO.Instance.Insert_CT_HocSinh_kyThiThu(ct);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+      
+        #endregion
+    }
+}
