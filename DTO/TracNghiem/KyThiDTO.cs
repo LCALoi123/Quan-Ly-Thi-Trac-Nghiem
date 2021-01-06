1c1ea9de862a94aadf522ad4a1bf4ded962f409e
Lê Công Anh Lợi <45191783+LCALoi123@users.noreply.github.com>
2021-01-06 23:16:20 +0700

    Add files via upload

---
+++ DTO/TracNghiem/KyThiDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{ 
+    public class KyThiDTO
+    {
+        private string maKyThi;
+        private string tenKyThi;
+        private string ngayThi;
+
+        public string MaKyThi { get => maKyThi; set => maKyThi = value; }
+        public string TenKyThi { get => tenKyThi; set => tenKyThi = value; }
+        public string NgayThi { get => ngayThi; set => ngayThi = value; }
+    }
+}
