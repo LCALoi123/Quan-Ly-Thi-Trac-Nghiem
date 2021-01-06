820dc8cf8b56e89e1471e22158e1cc8ad6e5d4c0
Lê Công Anh Lợi <45191783+LCALoi123@users.noreply.github.com>
2021-01-06 22:53:28 +0700

    Add files via upload

---
+++ DTO/Properties/User/AdminDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class AdminDTO
+    {
+        /// <summary>
+        /// iDus là id được liên kết khóa ngoại với id trong account để phân biệt các account với nhau
+        /// iDaddmin là id của addmin để phân biệt các addmin trong cùng bảng
+        /// </summary>
+        private int iDus;
+        private int iDadmin;
+        private string HoTen;
+        private string NgaySinh;
+        private string UserName;
+
+        //--------------------------------------------------------------------------------//
+        public int IDadmin { get => iDadmin; set => iDadmin = value; }
+        public int IDus { get => iDus; set => iDus = value; }
+
+        public string Hoten{ get => HoTen; set => HoTen = value; }
+        public string Ngaysinh { get => NgaySinh; set => NgaySinh = value; }
+        public string Username { get => UserName; set => UserName = value; }
+
+
+    }
+}
---
+++ DTO/Properties/User/HocSinhDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HocSinhDTO
+    {
+        /// <summary>
+        /// idHS là id của học sinh để phân biệt các học sinh với nhau
+        /// tenHS là tên của học sinh đó
+        /// LopHoc là lớp học hiện tại mà học sinh đó đang học
+        /// KhoiLop là Khối lớp học của lớp hiện tại
+        /// ngaySinh là ngày sinh của học sinh đó
+        /// </summary>
+        private int idHS;
+        private string tenHS;
+        private string lopHoc;
+        private string khoiLop;
+        private string ngaySinh;
+        private int iDus;
+
+        /***----------------------------------------------------------------------***/
+        public int IdHS { get => idHS; set => idHS = value; }
+        public string TenHS { get => tenHS; set => tenHS = value; }
+        public string KhoiLop { get => khoiLop; set => khoiLop = value; }
+        public string NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public int IDus { get => iDus; set => iDus = value; }
+        public string LopHoc { get => lopHoc; set => lopHoc = value; }
+    }
+}
