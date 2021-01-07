0fedd5215860412eadc04b7eef4eb8c97261e971
mrhai69 <58458520+mrhai69@users.noreply.github.com>
2021-01-07 23:49:07 +0700

    Add files via upload

---
+++ GUI/HocSinh/frInBangDiem.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GUI.HocSinh
+{
+    partial class frInBangDiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.rpvBangDiem = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.BangDiemDTOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.HocSinhDTOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BangDiemDTOBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HocSinhDTOBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rpvBangDiem
+            // 
+            this.rpvBangDiem.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "BangDiem";
+            reportDataSource1.Value = this.BangDiemDTOBindingSource;
+            reportDataSource2.Name = "ThongTinHocSinh";
+            reportDataSource2.Value = this.HocSinhDTOBindingSource;
+            this.rpvBangDiem.LocalReport.DataSources.Add(reportDataSource1);
+            this.rpvBangDiem.LocalReport.DataSources.Add(reportDataSource2);
+            this.rpvBangDiem.LocalReport.ReportEmbeddedResource = "GUI.Report.reportKetQuaHocSinh.rdlc";
+            this.rpvBangDiem.Location = new System.Drawing.Point(0, 0);
+            this.rpvBangDiem.Name = "rpvBangDiem";
+            this.rpvBangDiem.ServerReport.BearerToken = null;
+            this.rpvBangDiem.Size = new System.Drawing.Size(482, 558);
+            this.rpvBangDiem.TabIndex = 0;
+            // 
+            // BangDiemDTOBindingSource
+            // 
+            this.BangDiemDTOBindingSource.DataSource = typeof(DTO.BangDiemDTO);
+            // 
+            // HocSinhDTOBindingSource
+            // 
+            this.HocSinhDTOBindingSource.DataSource = typeof(DTO.HocSinhDTO);
+            // 
+            // frInBangDiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 558);
+            this.Controls.Add(this.rpvBangDiem);
+            this.Name = "frInBangDiem";
+            this.Text = "frInBangDiem";
+            this.Load += new System.EventHandler(this.frInBangDiem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BangDiemDTOBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HocSinhDTOBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rpvBangDiem;
+        private System.Windows.Forms.BindingSource BangDiemDTOBindingSource;
+        private System.Windows.Forms.BindingSource HocSinhDTOBindingSource;
+    }
+}
---
+++ GUI/HocSinh/frInBangDiem.cs
@@ -0,0 +1,57 @@
+﻿using BUS;
+using DAO;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.HocSinh
+{
+    public partial class frInBangDiem : Form
+    {
+        public frInBangDiem()
+        {
+            InitializeComponent();
+        }
+        static string name = "";
+        public frInBangDiem(string usname):this()
+        {
+            name = usname;
+        }
+        private void frInBangDiem_Load(object sender, EventArgs e)
+        {
+
+            load();
+        }
+        private void load()
+        {
+            rpvBangDiem.LocalReport.DataSources.Clear();
+            var DiemBS = new BindingSource();
+            var rDS = new ReportDataSource("BangDiem");
+            rDS.Value = DiemBS;
+            rpvBangDiem.LocalReport.DataSources.Add(rDS);
+            
+            int idus = AccountBUS.Instance.get_ID_account(name);
+
+            DiemBS.DataSource = DiemDAO.Instance.LayDSKetQua(HocSinhBUS.Instance.SelectIDhs(idus));
+
+            var HocSinhBS = new BindingSource();
+            var hsrDs = new ReportDataSource("ThongTinHocSinh");
+            hsrDs.Value = HocSinhBS;
+            rpvBangDiem.LocalReport.DataSources.Add(hsrDs);
+            HocSinhBS.DataSource = HocSinhBUS.Instance.get_HocSinh(name);
+
+
+
+            this.rpvBangDiem.RefreshReport();
+
+
+        }
+    }
+}
