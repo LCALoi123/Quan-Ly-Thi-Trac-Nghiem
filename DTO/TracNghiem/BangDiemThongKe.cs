ccc7d1acecbb5498b45802b19df5f24b9fc33b47
mrhai69 <58458520+mrhai69@users.noreply.github.com>
2021-01-07 23:42:57 +0700

    Add files via upload

---
+++ DTO/TracNghiem/BangDiemThongKe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BangDiemThongKe
+    {
+        private string kyThi;
+        private string deThi;
+        private float diem;
+        private int idHS;
+        private string tenHS;
+        private string lopHoc;
+        private string khoiLop;
+        private DateTime ngaySinh;
+        public string KyThi { get => kyThi; set => kyThi = value; }
+        public string DeThi { get => deThi; set => deThi = value; }
+        public float Diem { get => diem; set => diem = value; }
+        public int IdHS { get => idHS; set => idHS = value; }
+        public string TenHS { get => tenHS; set => tenHS = value; }
+        public string LopHoc { get => lopHoc; set => lopHoc = value; }
+        public string KhoiLop { get => khoiLop; set => khoiLop = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+    }
+}
---
+++ DTO/TracNghiem/DiemDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DiemDTO
+    {
+        private string maKyThi;
+        private string maDe;
+        private int idHS;
+        private float diem;
+        private int lanThi;
+        
+        public int IdHS { get => idHS; set => idHS = value; }
+        public float Diem { get => diem; set => diem = value; }
+        public string MaKyThi { get => maKyThi; set => maKyThi = value; }
+        public string MaDe { get => maDe; set => maDe = value; }
+        public int LanThi { get => lanThi; set => lanThi = value; }
+    }
+}
