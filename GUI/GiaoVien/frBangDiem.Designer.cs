9983f87192be7f7d74a41ab718d1348985700af4
mrhai69 <58458520+mrhai69@users.noreply.github.com>
2021-01-07 23:47:43 +0700

    Add files via upload

---
+++ GUI/GiaoVien/frBangDiem.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GUI.GiaoVien
+{
+    partial class frBangDiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.rpvBangDiem = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.BangDiemThongKeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BangDiemThongKeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rpvBangDiem
+            // 
+            this.rpvBangDiem.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "BangDiem";
+            reportDataSource1.Value = this.BangDiemThongKeBindingSource;
+            this.rpvBangDiem.LocalReport.DataSources.Add(reportDataSource1);
+            this.rpvBangDiem.LocalReport.ReportEmbeddedResource = "GUI.Report.reportBangDiem.rdlc";
+            this.rpvBangDiem.Location = new System.Drawing.Point(0, 0);
+            this.rpvBangDiem.Name = "rpvBangDiem";
+            this.rpvBangDiem.ServerReport.BearerToken = null;
+            this.rpvBangDiem.Size = new System.Drawing.Size(932, 516);
+            this.rpvBangDiem.TabIndex = 0;
+            // 
+            // BangDiemThongKeBindingSource
+            // 
+            this.BangDiemThongKeBindingSource.DataSource = typeof(DTO.BangDiemThongKe);
+            // 
+            // frBangDiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 516);
+            this.Controls.Add(this.rpvBangDiem);
+            this.Name = "frBangDiem";
+            this.Text = "frBangDiem";
+            this.Load += new System.EventHandler(this.frBangDiem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BangDiemThongKeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rpvBangDiem;
+        private System.Windows.Forms.BindingSource BangDiemThongKeBindingSource;
+    }
+}
---
+++ GUI/GiaoVien/frBangDiem.cs
@@ -0,0 +1,42 @@
+﻿using BUS;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.GiaoVien
+{
+    public partial class frBangDiem : Form
+    {
+        public frBangDiem()
+        {
+            InitializeComponent();
+        }
+        static string makt = "NULL";
+        public frBangDiem(string ma):this()
+        {
+            makt = ma;
+        }
+        private void frBangDiem_Load(object sender, EventArgs e)
+        {
+            
+            rpvBangDiem.LocalReport.DataSources.Clear();
+            var BangDiemDS = new BindingSource();
+            var hsrDs = new ReportDataSource("BangDiem");
+            hsrDs.Value = BangDiemDS;
+            rpvBangDiem.LocalReport.DataSources.Add(hsrDs);
+            if(makt == "NULL")
+                BangDiemDS.DataSource = DiemBUS.Instance.LayBangDiem();
+            else
+                BangDiemDS.DataSource = DiemBUS.Instance.LayBangDiem(makt);
+
+            this.rpvBangDiem.RefreshReport();
+        }
+    }
+}
