b907ff15560a81ca26db8f67a292a440648c556c
mrhai69 <58458520+mrhai69@users.noreply.github.com>
2021-01-07 23:41:54 +0700

    Add files via upload

---
+++ DTO/TracNghiem/BangDiemDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BangDiemDTO
+    {
+        private string kyThi;
+        private string deThi;
+        private float diem;
+
+        public string KyThi { get => kyThi; set => kyThi = value; }
+        public string DeThi { get => deThi; set => deThi = value; }
+        public float Diem { get => diem; set => diem = value; }
+    }
+}
