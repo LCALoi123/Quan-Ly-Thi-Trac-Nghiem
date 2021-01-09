7dfd071db3831c1d9673dab1799008e4a4815be2
Lê Anh Kiệt <58032294+Anhkiet99@users.noreply.github.com>
2021-01-09 19:28:39 +0700

    Add files via upload

---
+++ BUS/TracNghiem/CauHoiBUS.cs
@@ -0,0 +1,156 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class CauHoiBUS
+    {
+        #region singleton
+        private static CauHoiBUS instance;
+
+        public static CauHoiBUS Instance
+        {
+            get { if (instance == null) instance = new CauHoiBUS(); return CauHoiBUS.instance; }
+            private set { CauHoiBUS.instance = value; }
+        }
+        private CauHoiBUS() { }
+        #endregion
+
+        #region method
+        public bool Insert_CauHoi(CauHoiDTO cauHoi)
+        {
+            try
+            {
+                if (CauHoiDAO.Instance.KiemTraNoiDungCauHoi(cauHoi.CauHoi))
+                {
+                    CauHoiDAO.Instance.Add_CauHoi(cauHoi);
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        public void Insert_DongGopCauHoi(CauHoiDongGopDTO cauHoi)
+        {
+            try
+            {
+                CauHoiDAO.Instance.Add_DongGopCauHoi(cauHoi);
+            }
+            catch
+            {
+
+            }
+        }
+
+        public DataTable SelectCauHois()
+        {
+            try
+            {
+                return MethodBUS.Instance.ConvertToDataTable(CauHoiDAO.Instance.Select_CauHois());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public DataTable SelectCauHois(string MaDe)
+        {
+            try
+            {
+                return MethodBUS.Instance.ConvertToDataTable(CauHoiDAO.Instance.Select_CauHois(MaDe));
+            }
+            catch
+            {
+                return null;
+            }
+
+
+        }
+        public DataTable SelectCauHoiDongGops(int idhs)
+        {
+            try
+            {
+                return MethodBUS.Instance.ConvertToDataTable(CauHoiDAO.Instance.Select_CauHoiDongGop(idhs));
+            }
+            catch
+            {
+                return null;
+            }
+
+
+        }
+        public DataTable SelectCauHoiDongGops()
+        {
+            try
+            {
+                return MethodBUS.Instance.ConvertToDataTable(CauHoiDAO.Instance.Select_CauHoiDongGop());
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public CauHoiDTO SelectCauHoi(int idch)
+        {
+            try
+            {
+                CauHoi ch = CauHoiDAO.Instance.SelectCauHoi(idch);
+                CauHoiDTO cauhoi = new CauHoiDTO();
+                cauhoi.IdCH = ch.IDch;
+                cauhoi.CauHoi = ch.CauHoi1;
+                cauhoi.DapAnA = ch.dapanA;
+                cauhoi.DapAnB = ch.dapanB;
+                cauhoi.DapAnC = ch.dapanC;
+                cauhoi.DapAnD = ch.dapanD;
+                cauhoi.DapAnDung = ch.dapandung;
+                cauhoi.DoKho = ch.DoKho;
+                return cauhoi;
+
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public void update_CauHoi(CauHoiDTO cauHoi)
+        {
+            try
+            {
+                CauHoiDAO.Instance.Update_CauHoi(cauHoi);
+            }
+            catch
+            {
+
+            }
+
+        }
+        public void update_CauHoiDongGop(int id)
+        {
+            try
+            {
+                CauHoiDAO.Instance.Update_CauHoiDongGop(id);
+            }
+            catch
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ BUS/TracNghiem/DeBUS.cs
@@ -0,0 +1,116 @@
+﻿using DAO;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class DeBUS
+    {
+        #region singleton
+        private static DeBUS instance;
+
+        public static DeBUS Instance
+        {
+            get { if (instance == null) instance = new DeBUS(); return DeBUS.instance; }
+            private set { DeBUS.instance = value; }
+        }
+        private DeBUS() { }
+        #endregion
+        #region method
+        public void Insert_DeThi(DeThiDTO de)
+        {
+            try
+            {
+                DeDAO.Instance.Add_DeThi(de);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        public void Insert_CT_DeThi(CT_DethiDTO ct)
+        {
+            try
+            {
+                DeDAO.Instance.Add_CT_DeThi(ct);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        public List<int> selectDanhSachIDCH(string MaDe)
+        {
+            try
+            {
+                return DeDAO.Instance.select_idch(MaDe);
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public DataTable SelectDethis()
+        {
+            try
+            {
+                return MethodBUS.Instance.ConvertToDataTable(DeDAO.Instance.Select_DeThi());
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public List<string> SelectDanhSachTenDe(string makt)
+        {
+            try
+            {
+                return DeDAO.Instance.select_DS_TenDe(makt);
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+        public List<string> SelectDanhSachTenDeThiThu(string makt)
+        {
+            try
+            {
+                return DeDAO.Instance.select_DS_TenDeThiThu(makt);
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public List<string> LayDSMaDe()
+        {
+            try
+            {
+                return DeDAO.Instance.LayDanhSachMaDe();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
